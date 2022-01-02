80679df834e502c872342dd5d448687eddc477e0
Oh Dylan <ddragon1128@hotmail.com>
2022-01-02 12:10:38 +0800

    Setup front and backend backbone and dependencies

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://doh:<password>@cluster0.earoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
